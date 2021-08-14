5c37798db255f52f4e72341eccefb1d66187f0cd
Yunfei He <i.heyunfei@gmail.com>
2021-08-14 20:24:49 +0800

    init

---
+++ cra/src/App.jsx
@@ -0,0 +1,24 @@
+/* @refresh reset */
+
+import React, { useState } from "react";
+import Foo from './components/FunctionFoo'
+import Bar from './components/ClassBar'
+const LazyBar = React.lazy(() => import("./components/ClassBar"));
+const LazyFoo = React.lazy(() => import("./components/FunctionFoo"));
+function App() {
+  return (
+    <>
+    <h1>dynamic import</h1>
+      <React.Suspense fallback={'loading bar'}>
+        <LazyBar />
+      </React.Suspense>
+      <React.Suspense fallback={'loading foo'}>
+        <LazyFoo />
+      </React.Suspense>
+      <h1>static import</h1>
+      <Bar />
+      <Foo />
+    </>
+  );
+}
+export default App;
---
+++ vite/src/components/FunctionFoo.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default () => (
+  <div>foo2</div>
+)
